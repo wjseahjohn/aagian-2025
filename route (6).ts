32b33efe6a2783c0e3e0d1a49d835529c3d37efb
wjseahjohn <wjseahjohn@gmail.com>
2026-07-02 16:52:32 +0800

    Delete route (6).ts

--- route (6).ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { COOKIE } from '@/lib/session'
-
-export async function POST() {
-  const res = NextResponse.json({ success: true })
-  res.cookies.set(COOKIE, '', { maxAge: 0, path: '/' })
-  return res
-}
